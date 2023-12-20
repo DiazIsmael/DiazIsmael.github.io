ca1672bc27d2866e11a691e30644b2aabbae87a1
ismael <ismaelturtle@gmail.com>
2023-12-20 02:02:04 -0600

    trying stuff

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue()],
+  plugins: [],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
